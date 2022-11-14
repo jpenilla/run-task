0d91096e8a659f757aef1ce01c1120ddc5a42107
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2022-11-14 11:00:07 -0700

    Add `run-waterfall` and `cleanAllRunTaskCaches`

--- src/main/kotlin/xyz/jpenilla/runtask/util/Constants.kt
+++ src/main/kotlin/xyz/jpenilla/runtask/util/Constants.kt
@@ -20,12 +20,14 @@ internal object Constants {
   const val SHARED_TASK_GROUP = "Run Task Shared"
   const val RUN_PAPER_TASK_GROUP = "Run Paper"
   const val RUN_VELOCITY_TASK_GROUP = "Run Velocity"
+  const val RUN_WATERFALL_TASK_GROUP = "Run Waterfall"
 
   const val GRADLE_CACHES_DIRECTORY_NAME = "caches"
   const val RUN_PATH = "run-task-jars"
   const val USER_PATH = "$RUN_PATH/user"
   const val PAPER_PATH = "$RUN_PATH/paper"
   const val VELOCITY_PATH = "$RUN_PATH/velocity"
+  const val WATERFALL_PATH = "$RUN_PATH/waterfall"
 
   object Plugins {
     const val SHADOW_PLUGIN_ID = "com.github.johnrengelman.shadow"
@@ -38,16 +40,20 @@ internal object Constants {
   object Tasks {
     const val CLEAN_PAPERCLIP_CACHE = "cleanPaperclipCache"
     const val CLEAN_VELOCITY_CACHE = "cleanVelocityCache"
+    const val CLEAN_WATERFALL_CACHE = "cleanWaterfallCache"
     const val CLEAN_USER_SERVICES_CACHE = "cleanCustomServiceCaches"
+    const val CLEAN_ALL_CACHES = "cleanAllRunTaskCaches"
 
     const val RUN_SERVER = "runServer"
     const val RUN_MOJANG_MAPPED_SERVER = "runMojangMappedServer"
     const val RUN_VELOCITY = "runVelocity"
+    const val RUN_WATERFALL = "runWaterfall"
   }
 
   object Services {
     const val PAPER = "paper-download-service"
     const val VELOCITY = "velocity-download-service"
+    const val WATERFALL = "waterfall-download-service"
   }
 
   object Properties {
@@ -57,6 +63,10 @@ internal object Constants {
 
   object Extensions {
     const val RUN_PAPER = "runPaper"
-    const val RUN_VELOCITY = "runVelocityExtension" // Extension suffix needed to avoid conflict with task name...
+
+    // Extension suffix needed to avoid conflict with task name (Kotlin DSL prioritizes
+    // extension even when in TaskContainer scope)...
+    const val RUN_VELOCITY = "runVelocityExtension"
+    const val RUN_WATERFALL = "runWaterfallExtension"
   }
 }
---
+++ src/main/kotlin/xyz/jpenilla/runwaterfall/RunWaterfallPlugin.kt
@@ -0,0 +1,49 @@
+/*
+ * Run Task Gradle Plugins
+ * Copyright (c) 2021-2022 Jason Penilla
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package xyz.jpenilla.runwaterfall
+
+import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.register
+import xyz.jpenilla.runtask.RunExtension
+import xyz.jpenilla.runtask.RunPlugin
+import xyz.jpenilla.runtask.service.DownloadsAPIService
+import xyz.jpenilla.runtask.util.Constants
+import xyz.jpenilla.runtask.util.sharedCaches
+import xyz.jpenilla.runwaterfall.task.RunWaterfall
+
+public abstract class RunWaterfallPlugin : RunPlugin() {
+  override fun apply(target: Project) {
+    super.apply(target)
+
+    val runExtension = target.extensions.create<RunExtension>(Constants.Extensions.RUN_WATERFALL, target)
+    DownloadsAPIService.waterfall(target)
+
+    target.tasks.register<Delete>(Constants.Tasks.CLEAN_WATERFALL_CACHE) {
+      group = Constants.RUN_WATERFALL_TASK_GROUP
+      description = "Delete all locally cached Waterfall jars."
+      delete(target.sharedCaches.resolve(Constants.WATERFALL_PATH))
+    }
+
+    val runWaterfall = target.tasks.register<RunWaterfall>(Constants.Tasks.RUN_WATERFALL) {
+      group = Constants.RUN_WATERFALL_TASK_GROUP
+      description = "Run a Waterfall server for plugin testing."
+    }
+    runWaterfall.setupPluginJarDetection(target, runExtension)
+  }
+}
