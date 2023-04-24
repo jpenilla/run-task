6bb19ac7f0524cf14f898417a4ec79265be6b65a
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-04-23 18:28:13 -0700

    release: Version 2.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "xyz.jpenilla"
-version = "2.1.0-SNAPSHOT"
+version = "2.1.0"
 description = "Gradle plugins adding run tasks for Minecraft server and proxy software"
 
 repositories {
