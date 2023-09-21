553b947a7cc58ec991252cde3e3f901441999ebc
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-09-20 18:56:16 -0700

    Hangar works

--- tester/build.gradle.kts
+++ tester/build.gradle.kts
@@ -11,8 +11,7 @@ runPaper.folia.registerTask()
 val paperPlugins = runPaper.downloadPluginsSpec {
   modrinth("carbon", "2.1.0-beta.21")
   github("jpenilla", "MiniMOTD", "v2.0.13", "minimotd-bukkit-2.0.13.jar")
-  github("jpenilla", "squaremap", "v1.2.0", "squaremap-paper-mc1.20.1-1.2.0.jar")
-  // hangar("squaremap", "1.2.0")
+  hangar("squaremap", "1.2.0")
   url("https://download.luckperms.net/1515/bukkit/loader/LuckPerms-Bukkit-5.4.102.jar")
 }
 
@@ -26,7 +25,8 @@ tasks {
     version("3.2.0-SNAPSHOT")
     runDirectory.set(layout.projectDirectory.dir("runVelocity"))
     downloadPlugins {
-      modrinth("carbon", "wn7XQHfd")
+      modrinth("minimotd", "OQpVrXXW")
+      hangar("Carbon", "2.1.0-beta.21")
       url("https://download.luckperms.net/1515/velocity/LuckPerms-Velocity-5.4.102.jar")
     }
   }
