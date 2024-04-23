ceb409810436c2f59903edb7bef73992e017635a
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-04-22 18:40:51 -0700

    Update Gradle wrapper to 8.7 and add toolchains plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,10 @@ pluginManagement {
   includeBuild("plugin")
 }
 
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
 rootProject.name = "run-task-parent"
 
 include("tester")
