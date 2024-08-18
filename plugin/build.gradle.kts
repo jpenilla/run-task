e9c4daec7b4d3143723fa2dae0f26dd644cfdf42
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2024-08-18 14:11:46 -0700

    2.3.2-SNAPSHOT

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "xyz.jpenilla"
-version = "2.3.1"
+version = "2.3.2-SNAPSHOT"
 description = "Gradle plugins adding run tasks for Minecraft server and proxy software"
 
 repositories {
