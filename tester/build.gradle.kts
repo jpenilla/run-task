fc32a2f561eb1a593dceb383cdb4465c906a7b84
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-07-04 17:41:53 -0700

    Fix tester

--- tester/build.gradle.kts
+++ tester/build.gradle.kts
@@ -1,17 +1,24 @@
 import xyz.jpenilla.runpaper.task.RunServer
 
 plugins {
+  java
   id("xyz.jpenilla.run-paper")
   id("xyz.jpenilla.run-velocity")
   id("xyz.jpenilla.run-waterfall")
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
 }
 
 runPaper.folia.registerTask()
 
 val paperPlugins = runPaper.downloadPluginsSpec {
   modrinth("carbon", "6dmNHzy8")
-  github("jpenilla", "MiniMOTD", "v2.1.0", "minimotd-bukkit-2.1.0.jar")
-  hangar("squaremap", "1.2.3")
+  github("jpenilla", "MiniMOTD", "v2.1.6", "minimotd-bukkit-2.1.6.jar")
+  hangar("squaremap", "1.3.6")
   url("https://download.luckperms.net/1593/bukkit/loader/LuckPerms-Bukkit-5.5.8.jar")
 }
 
@@ -22,12 +29,12 @@ tasks {
     downloadPlugins.from(paperPlugins)
   }
   runVelocity {
-    version("3.3.0-SNAPSHOT")
+    version("3.4.0-SNAPSHOT")
     runDirectory.set(layout.projectDirectory.dir("runVelocity"))
     downloadPlugins {
       modrinth("minimotd", "z8DFFJMR")
       hangar("Carbon", "3.0.0-beta.26")
-      url("https://download.luckperms.net/1530/velocity/LuckPerms-Velocity-5.4.117.jar")
+      url("https://download.luckperms.net/1594/velocity/LuckPerms-Velocity-5.5.9.jar")
     }
   }
   runWaterfall {
